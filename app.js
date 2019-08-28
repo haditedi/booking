39728bef0884deb0c61e5067d5507e67127642a2
Hadi Tedi <hadit@club21.co.uk>
2019-08-28 15:03:31 +0100

    revision .env

--- app.js
+++ app.js
@@ -122,34 +122,34 @@ app.post("/test", (req,res) => {
   
   doc.end();
 
-  // async function sendingMail(){
-  //   let transporter = nodemailer.createTransport({
-  //     host: "smtp.office365.com",
-  //     port: 587,
-  //     secure: false, 
-  //     auth: {
-  //       user: process.env.USER, 
-  //       pass: process.env.PASS 
-  //     },
-  //   });
+  async function sendingMail(){
+    let transporter = nodemailer.createTransport({
+      host: "smtp.office365.com",
+      port: 587,
+      secure: false, 
+      auth: {
+        user: process.env.USER, 
+        pass: process.env.PASS 
+      },
+    });
   
-  //   let info = await transporter.sendMail({
-  //     from: "sales@monarchhouse.co.uk", 
-  //     to: email, 
-  //     subject: "Monarch House confirmation for "+name, // Subject line
-  //     text: "Thank you for your booking",
-  //     attachments: [
-  //         {   
-  //             filename: filename+'.pdf',
-  //             path: 'public/confirmation/'+filename+'.pdf'
-  //         }]
+    let info = await transporter.sendMail({
+      from: process.env.FROMEMAIL, 
+      to: email, 
+      subject: "Monarch House confirmation for "+name, // Subject line
+      text: "Thank you for your booking",
+      attachments: [
+          {   
+              filename: filename+'.pdf',
+              path: 'public/confirmation/'+filename+'.pdf'
+          }]
       
-  //   });
+    });
   
-  //   console.log("Message sent: %s", info.messageId);
-  // }
+    console.log("Message sent: %s", info.messageId);
+  }
   
-  // sendingMail();
+  sendingMail();
 
   res.render("test", {test: filename});
     
